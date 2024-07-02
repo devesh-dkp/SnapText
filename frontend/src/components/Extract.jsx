ea49a06f311fb8178d94dd78373e09d6c5ef8f3b
devesh-dkp <deveshpandey.dp2+git@gmail.com>
2024-07-02 15:42:06 +0530

    "new commit"

--- frontend/src/components/Extract.jsx
+++ frontend/src/components/Extract.jsx
@@ -53,16 +53,13 @@ function Extract() {
     });
   };
   const handleExport = () => {
-    // Check if text is an array before processing
- 
-
     // Convert the list to CSV format
-    let csv = '';
+    let csv = "";
     csv += text;
 
     // Create a Blob from the CSV string
     const element = document.createElement("a");
-    const fileBlob = new Blob([csv], { type: 'text/csv' });
+    const fileBlob = new Blob([csv], { type: "text/csv" });
     element.href = URL.createObjectURL(fileBlob);
     element.download = `${category}-${Date.now()}.csv`;
     document.body.appendChild(element);
@@ -70,19 +67,23 @@ function Extract() {
     document.body.removeChild(element);
   };
 
-
   return (
     <>
-      <div className="flex justify-center font-bold text-black bg-white font-bold text-5xl p-5 py-16 items-center">
+      <div className="flex justify-center font-bold text-black bg-white font-bold text-5xl p-5 pt-16 items-center">
         Upload Image
       </div>
       <div className="flex flex-col items-center">
         {phase === "choose" && (
           <>
-            <label
-              for="dropzone-file"
-              className="flex flex-col items-center justify-center w-1/2 h-64 border-2 border-gray-400 rounded-lg cursor-pointer bg-purple-100 hover:bg-purple-200"
-            >
+            <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 m-2 p-1">
+              <p className="font-bold">Caution</p>
+              <p>
+                Please upload an image with high resolution and cropped to the
+                area of interest.
+              </p>
+            </div>
+
+            <label className="flex flex-col items-center justify-center w-1/2 px-4 h-64 border-2 my-2 border-gray-400 rounded-lg cursor-pointer bg-purple-100 hover:bg-purple-200">
               {file ? (
                 <div className="w-full h-full flex items-center justify-center">
                   {file.name}
@@ -95,19 +96,11 @@ function Extract() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                    />
-                  </svg>
+                  ></svg>
 
                   <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span class="font-semibold">Click to upload</span> or drag
-                    and drop
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
                   </p>
                   <p className="text-xs text-gray-500 dark:text-gray-400">
                     SVG, PNG, JPG or GIF (MAX. 800x400px)
@@ -135,7 +128,7 @@ function Extract() {
               <option value="business-card">Business Card</option>
               <option value="menu">Menu</option>
               <option value="identity-card">Identity Card</option>
-              <option value="other">Other</option>
+              <option value="list">Some List</option>
             </select>
             <button
               onClick={handleUpload}
